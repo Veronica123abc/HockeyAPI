398ccedf614095b52646d8b3262001f1204cd716
Veronica Eriksson <veronica.eriksson@sogeti.com>
2022-11-20 08:44:22 +0100

    wip

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request, render_template, redirect, url_for
+import pandas as pd
+
 
 app = Flask(__name__)
 
@@ -7,6 +9,11 @@ app = Flask(__name__)
 def index():
    print('Request for index page received')
    return render_template('index.html')
+
+def print_csv(filename):
+    df = pd.read_csv(filename)
+    print(df)
+
 
 #@app.route('/hello', methods=['GET'])
 #def helloworld():
@@ -21,9 +28,16 @@ def upload_file():
 
     if uploaded_file.filename != '':
         print(uploaded_file.filename)
-        uploaded_file.save('kallekula.csv')#uploaded_file.filename)
+        uploaded_file.save('tmp.csv')#uploaded_file.filename)
+        print_csv('tmp.csv')
         #print(redirect(url_for('index')))
-    return redirect(url_for('index'))
+        df = pd.read_csv('tmp.csv')
+        text = df.columns.values[0]
+    return render_template('test.html', name=text)
+    #return redirect(url_for('test'))
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
